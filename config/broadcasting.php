bff3cd58e3739a56317241dd4276cf69be6e35a2
FERNANDO <fgolmos10@gmail.com>
2024-04-12 12:51:00 -0600

    sadasd

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => '192.168.117.161',
+                'host' => '18.227.105.11',
                 'port' => 6001,
                 'scheme' => 'http',
                 'encrypted' => true,
